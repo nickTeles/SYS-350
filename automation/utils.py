f17d5b231b6457a972650caf1ecdf2dd4262131f
nickTeles <nicholas.teles@mymail.champlain.edu>
2022-02-14 23:11:17 -0500

    initial commit

--- automation/utils.py
+++ automation/utils.py
@@ -4,6 +4,6 @@ import ssl
 passw = getpass.getpass()
 s = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
 s.verify_mode=ssl.CERT_NONE
-si = SmartConnect(host="vcenter.nicholas.local", user="nicholas-adm", pws=passw, SSLContext=s)
+si = SmartConnect(host="vcenter.nicholas.local", user="nicholas-adm", pwd=passw, sslContext=s)
 aboutInfo=si.content.about
 print(aboutInfo)
