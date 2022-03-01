a061b863102d62ff6308eea351708c626a55d7f8
nickTeles <nicholas.teles@mymail.champlain.edu>
2022-02-28 21:43:00 -0500

    initial commit

--- automation/utils.py
+++ automation/utils.py
@@ -1,9 +1,15 @@
+from distutils.command.config import config
 import getpass
+from html.entities import name2codepoint
+import imp
 from re import search
 from pyVim.connect import SmartConnect
 import ssl
 from pyVim import connect
 from pyVmomi import vim
+import atexit
+from pprint import pprint
+
 
 # Get password from user to log into vSphere
 passw = getpass.getpass()
@@ -17,19 +23,44 @@ print(aboutInfo.fullName,username)
 content = si.RetrieveContent()
 virtual_machines = content.viewManager.CreateContainerView(
     content.rootFolder, [vim.VirtualMachine], True)
-nameandip1 = {}
-nameandip2 = {}
+# name dictionary
+vmname = {}
+# ip dictionary
+ipaddr = {}
+# power dictionary
+powered = {}
+corecount = {}
+
+mem = {}
+
+toPowerOn = input("VM to power on: ")
+
 for virtual_machine in virtual_machines.view:
     name = virtual_machine.name
+    if (toPowerOn == name):
+        virtual_machine.PowerOff
+        print("Power")
+
     vmip = virtual_machine.guest.ipAddress
-    nameandip1[name] = name
-    nameandip2[name] = vmip
-    #print(nameandip1)
-    #print(virtual_machines.view)
+    #print (virtual_machine.summary)
+    power = "powered off"
+    if (vim.VirtualMachinePowerState.poweredOn == virtual_machine.runtime.powerState):
+        power = "powered on"
+    else:
+        power = "powered off"
+    #print (virtual_machine.hardware.cpuInfo)
+    vmname[name] = name
+    ipaddr[name] = vmip
+    powered[name] = power
+    corecount[name] = virtual_machine.summary.config.numCpu
+    mem[name] = virtual_machine.summary.config.memorySizeMB / 1024
+    #print (mem)
 searchfor = input("Search for: ")
-#print (nameandip[name])
-if searchfor in nameandip1:
-    print (nameandip1[searchfor], nameandip2[searchfor])
-else:
-    print ("Not in dictionary")
-    print (nameandip)
+print('{:<32}{:<32}{:<32}{:<32}{:<32}'.format("Name", "IP Address", "Power State", "Core Count", "Memory (GB)"))
+print('{:<32}{:<32}{:<32}{:<32}{:<32}'.format("----", "----------", "-----------", "----------", "-----------"))
+for key, val in vmname.items():
+    if searchfor in key:
+        print('{:<32}{:<32}{:<32}{:<32}{:<32}'.format(vmname[key], str(ipaddr[key]), powered[key], corecount[key], mem[key]))
+        #print(tabulate([vmname[key], ipaddr[key], powered[key], corecount[key], mem[key]], headers=[key]))
+
+
